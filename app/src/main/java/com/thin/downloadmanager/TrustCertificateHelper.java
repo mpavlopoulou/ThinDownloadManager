e21545c7856d05510ac12bafe03720b76d03474d
Maria Pavlopoulou <m.pavlopoulou@afse.eu>
2021-11-24 15:16:35 +0200

    ThinDownload Manager - Add function to set a policy for untrusted certificates

---
+++ app/src/main/java/com/thin/downloadmanager/TrustCertificateHelper.java
@@ -0,0 +1,50 @@
+package com.thin.downloadmanager;
+
+import android.annotation.SuppressLint;
+
+import com.thin.downloadmanager.util.Log;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * This Class used for test purpose only , when a certificate is untrusted and disables the SSL
+ * certificate checking for new instances of HttpsURLConnection
+ * DO NOT USE THIS IN PRODUCTION
+ */
+
+public class TrustCertificateHelper {
+
+    @SuppressLint("TrustAllX509TrustManager")
+    public void trustAllCertificates() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier((arg0, arg1) -> true);
+        } catch (Exception e) {
+            Log.e(e.getMessage());
+        }
+    }
+}
